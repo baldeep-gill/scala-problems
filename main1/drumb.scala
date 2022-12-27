cd8d7004cd3edde7dfc3231fcaa2451663bfa782
Baldeep Gill <baldeep.gill@kcl.ac.uk>
2022-12-27 16:37:50 +0000

    Fix implementation of exercise 1

--- main1/drumb.scala
+++ main1/drumb.scala
@@ -18,8 +18,9 @@ import scala.util._
 
 // (1) 
 def get_january_data(symbol: String, year: Int) : List[String] = {
-    Try(Source.fromFile(s"${symbol}.csv")("ISO-8859-1").getLines().toList).getOrElse(List()).filter(_.startsWith("2014"))
+    Try(Source.fromFile(s"${symbol}.csv")("ISO-8859-1").getLines().toList).getOrElse(List()).filter(_.startsWith(s"${year}"))
 }
+println(get_january_data("GOOG", 1980) == List())
 
 
 // (2) 
